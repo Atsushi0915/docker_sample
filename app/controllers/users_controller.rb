2fbd8af440dbb3bd1da69bda35d46638ae04bf89
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-03-27 12:39:08 +0900

    新規会員登録ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to('/users/index')
+      flash.notice = "新規会員登録しました。"
+    else
+      render('users/new')
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#top'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get  'users/new',   to:  'users#new'
+  get  'users/index', to:  'users#index'
+  post 'users/create', to: 'users#create'
 end
