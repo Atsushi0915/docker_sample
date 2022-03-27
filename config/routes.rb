79647843caf79a4d803a4a894d68dc4aac05c026
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-03-27 14:36:55 +0900

    ユーザー一覧ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#top'
   
-  get  'users/new',   to:  'users#new'
-  get  'users/index', to:  'users#index'
-  post 'users/create', to: 'users#create'
+  get  'users/new',    to:  'users#new'
+  get  'users/index',  to:  'users#index'
+  post 'users/create', to:  'users#create'
 end
