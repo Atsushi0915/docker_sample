4da297183d7e013a0c9442a7dc5b4ffa2490f539
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-03-29 07:08:58 +0900

    新規投稿ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,16 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = @current_user.posts.build(post_params)
+    @post.user_id = @current_user.id
+    if @post.save
+      flash.notice = "投稿しました"
+      redirect_to posts_index_path
+    else
+      flash.notice = "投稿失敗しました"
+      render post_new_path
+    end
+
   end
 
   def index
@@ -17,4 +27,9 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   
   root 'posts#top'
 
-  get '/login',  to:  'sessions#new',  as: 'login_form'
-  post '/login',  to: 'sessions#create', as:  'login'
-  get  '/logout', to:  'sessions#destroy',  as:  'logout'
+  get '/login',   to:  'sessions#new',     as: 'login_form'
+  post '/login',  to: 'sessions#create',   as:  'login'
+  get  '/logout', to:  'sessions#destroy', as:  'logout'
   
   get  'users/new',    to:  'users#new'
   get  'users/index',  to:  'users#index'
   post 'users/create', to:  'users#create'
 
-  get 'posts/new',   to:  'posts#new',  as: 'post_new'
+  get  'posts/new',     to: 'posts#new',     as:  'post_new'
   post 'posts/create',  to: 'posts#create',  as:  'post_create'
+  get  'posts/index',   to: 'posts#index',   as:  'posts_index'
 end
