3fd911d190a5edb1d4c8ed302540943771e5c14a
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-03-31 07:24:35 +0900

    topページ更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   root 'posts#top'
 
-  get '/login',   to:  'sessions#new',     as: 'login_form'
-  post '/login',  to: 'sessions#create',   as:  'login'
-  get  '/logout', to:  'sessions#destroy', as:  'logout'
+  get '/login',   to:  'sessions#new',      as: 'login_form'
+  post '/login',  to:  'sessions#create',   as:  'login'
+  get  '/logout', to:  'sessions#destroy',  as:  'logout'
   
   get  'users/new',    to:  'users#new'
   get  'users/index',  to:  'users#index'
